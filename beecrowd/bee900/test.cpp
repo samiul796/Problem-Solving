cdeee566456ee161e61eeea9d333fe45d0ba2096
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-06-04 03:44:50 +0600

    ok

---
+++ beecrowd/bee900/test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    cout << "Vast set of Vast Operations" << endl;
+
+    cout << "Press Enter to close" << "\n";
+
+    cin.get();
+    return 0;
+}
