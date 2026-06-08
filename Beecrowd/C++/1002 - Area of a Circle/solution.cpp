e82d92a4b788b09ef0c7e13eae95db2c410f57aa
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-06-08 10:38:51 +0600

    added

---
+++ Beecrowd/C++/1002 - Area of a Circle/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double R, n = 3.14159;
+    cin >> R;
+    cout << fixed << setprecision(4) << "A=" << n * R * R << endl;
+    return 0;
+}
+
