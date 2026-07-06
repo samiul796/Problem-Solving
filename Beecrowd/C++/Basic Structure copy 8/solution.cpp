032b83d3aef49a668efdf725bab5bddcc8d2db8f
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-07-06 20:41:04 +0600

    accepted

--- Beecrowd/C++/Basic Structure copy 8/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 8/solution.cpp
@@ -4,12 +4,24 @@ using namespace std;
 int main()
 {
 
+    vector<int> n(10);
 
-//
+    for (int kk = 0; kk < 10; kk++)
+    {
+        /* code */
 
-    
+        cin >> n[kk];
+    }
 
+    for (int kk = 0; kk < 10; kk++)
+    {
+        /* code */
 
+        if (n[kk] < 1)
+            cout << "X[" << kk << "] = " << 1 << endl;
+        else
+            cout << "X[" << kk << "] = " << n[kk] << endl;
+    }
 
     return 0;
 }
