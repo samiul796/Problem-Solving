b0e20f29b3fa3c1a98364bc0b1beedca0ad1e3a4
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-07-06 20:22:44 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 9/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 9/solution.cpp
@@ -4,15 +4,27 @@ using namespace std;
 int main()
 {
 
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int num;
+        cin >> num;
 
+        for (int kkk = 2; kkk < num; kkk++)
+        {
 
+            if ((num % kkk) == 0)
+            {
+                cout << num << " nao eh perfeito" << endl;
+            }
+        }
 
-    
-
-
+        if (sum == num)
+            cout << num << " eh perfeito" << endl;
+        else
+            cout << num << " nao eh perfeito" << endl;
+    }
 
     return 0;
-}
-
-
-//
+} 
