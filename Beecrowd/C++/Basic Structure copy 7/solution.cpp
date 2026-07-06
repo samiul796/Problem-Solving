aeed67abb9938ae14a95d4dabadc30b8f0bbfb06
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-07-06 21:11:33 +0600

    added

--- Beecrowd/C++/Basic Structure copy 7/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 7/solution.cpp
@@ -4,12 +4,22 @@ using namespace std;
 int main()
 {
 
+    vector<double> n(100);
 
+    for (int kk = 0; kk < 100; kk++)
+    {
 
+        cin >> n[kk];
+    }
 
-    
-//
+    for (int kk = 0; kk < 100; kk++)
+    {
 
+        if (n[kk] <= 10.0)
+            cout << "A[" << kk << "] = " << showpoint << fixed << setprecision(1) << n[kk] << endl;
+        else
+            continue;
+    }
 
     return 0;
 }
