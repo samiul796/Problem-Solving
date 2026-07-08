270815cbd225029ed1769412876a99376c08d1c6
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-08 23:43:31 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 6/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 6/solution.cpp
@@ -6,9 +6,18 @@ int main()
 
     double n;
     cin >> n;
-    vector<double>
+    vector<double> vec;
+    vec.push_back(n);
 
-        //
+    while (99 --)
+    {
+        vec.push_back(n / 2.0);
+    }
 
-        return 0;
+    for (auto x : vec)
+    {
+        cout << x << endl;
+    }
+
+    return 0;
 }
