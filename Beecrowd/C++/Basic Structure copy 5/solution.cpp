f60e8bdf1675ae17b4efa65959d2a2a2ad6ff713
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-15 14:43:39 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 5/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 5/solution.cpp
@@ -4,20 +4,5 @@ using namespace std;
 int main()
 {
 
-    double cyy2 = 2.9893 ;
- 
-    
-
-    int cyy2236332 = (int)cyy2 ; 
-
-
-    int bbbb= 67; 
-
-    double jj8 = (double) bbbb ;
-
-
-    cout << cyy2 << endl;
-
-
     return 0;
 }
