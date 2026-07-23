3b9d5d3714071a6048be5250c3506beba0032b2a
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-23 18:06:02 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 7/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 7/solution.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int main()
 {
 
+    int n;
+    cin >> n;
+
+    vector<int> vec(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vec[];
+    }
+
+    
+
     return 0;
 }
