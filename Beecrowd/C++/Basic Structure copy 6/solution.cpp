c2a9ae94643430d62d51de6ee4646d3c7a4e8052
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-23 18:41:53 +0600

    ok

--- Beecrowd/C++/Basic Structure copy 6/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 6/solution.cpp
@@ -5,17 +5,36 @@ int main()
 {
 
     int n;
-    cin >> n;
-    double arr[11][11];
+    char p;
+    cin >> n >> p;
+    double arr[12][12];
 
-    for (int k = 0; k < count; k++)
+    for (int k = 0; k < 12; k++)
     {
-        for (int kk = 0; kk < count; kk++)
+        for (int kk = 0; kk < 12; kk++)
         {
-
-            //
-            //
+            cin >> arr[k][kk];
         }
+    }
+    if (p == 'S')
+    {
+        double sum = 0;
+
+        for (int kk = 0; kk < 12; kk++)
+        {
+            sum += arr[n][kk];
+        }
+        cout << sum << endl;
+    }
+    else if (p == 'M')
+    {
+        double sum = 0;
+
+        for (int kk = 0; kk < 12; kk++)
+        {
+            sum += arr[n][kk];
+        }
+        cout << (sum / 12.0) << endl;
     }
 
     return 0;
