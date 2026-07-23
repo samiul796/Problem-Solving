32168b38a4e00837fda14303677fcc332d2d02b8
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-23 18:29:02 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 6/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 6/solution.cpp
@@ -4,17 +4,19 @@ using namespace std;
 int main()
 {
 
+    int n;
+    cin >> n;
+    double arr[11][11];
 
+    for (int k = 0; k < count; k++)
+    {
+        for (int kk = 0; kk < count; kk++)
+        {
 
-
-
+            //
+            //
+        }
+    }
 
     return 0;
 }
-
-
-
-
-
-
-
