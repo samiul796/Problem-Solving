f0268ed53d5d7c5f03aa3bc16a3e94438df60118
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-24 16:37:18 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 4/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 4/solution.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main()
 {
 
+    int c;
+    cin >> c;
 
-
-
-    
-
-
+    while (c--)
+    {
+        int a, b;
+        cout << a + b << endl;
+    }
 
     return 0;
 }
