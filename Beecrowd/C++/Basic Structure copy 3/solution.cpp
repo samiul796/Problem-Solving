dbca6ad7880a22341f964607601640db480b9cfb
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-24 14:14:31 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 3/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 3/solution.cpp
@@ -3,13 +3,40 @@ using namespace std;
 
 int main()
 {
+    char p;
+    cin >> p;
 
+    double arr[12][12];
 
+    for (int k = 0; k < 12; k++)
+    {
+        for (int kk = 0; kk < 12; kk++)
+        {
+            cin >> arr[k][kk];
+        }
+    }
 
+    double sum = 0;
 
-    
+    for (int k = 1; k <= 5; k++)
+    {
+        for (int kk = 0; kk < k; kk++)
+        {
+            sum += arr[k][kk];      
+            sum += arr[11 - k][kk]; 
+        }
+    }
 
+    cout << fixed << setprecision(1);
 
+    if (p == 'S')
+    {
+        cout << sum << endl;
+    }
+    else
+    {
+        cout << sum / 30.0 << endl;
+    }
 
     return 0;
 }
