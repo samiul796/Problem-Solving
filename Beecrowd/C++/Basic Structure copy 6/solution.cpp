dc5e168200dd2177da4ae3c8963d6d1843f1d805
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-24 16:14:36 +0600

    ok

--- Beecrowd/C++/Basic Structure copy 6/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 6/solution.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main()
 {
 
+    double a, b, c;
+    cin >> a >> b >> c;
 
-
-
-    
-
-
+    while ((cin >> a >> b >> c) && (a != 0) && (b != 0) && (c != 0))
+    {
+        double res = (a * b * 100) / c;
+        int finalans = (int)sqrt(res);
+        cout << finalans << endl;
+    }
 
     return 0;
 }
