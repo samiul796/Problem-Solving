1f981089eb078ba0ac60506562864a0849320482
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-24 16:22:38 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 6/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 6/solution.cpp
@@ -5,14 +5,13 @@ int main()
 {
 
     double a, b, c;
-    cin >> a >> b >> c;
 
     while (cin >> a >> b >> c)
     {
         if (a == 0)
             break;
 
-        double res = (a * b * 100) / c;
+        double res = (a * b * 100.0) / c;
         cout << (int)sqrt(res) << endl;
     }
     return 0;
