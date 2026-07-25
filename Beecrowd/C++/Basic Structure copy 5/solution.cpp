bb684d269383aee5eff51b86bae28a85a27539f4
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-25 13:46:52 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 5/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 5/solution.cpp
@@ -1,40 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int n;
     cin >> n;
 
-    for (n--)
+    for (int tc = 1; tc <= n; tc++)
     {
-        int Sheldon, raj;
-        //  scissors ,paper, rock , lizard ,Spock
-        string word1, word2, array1[5] = {"pedra", "papel", "tesoura", "lagarto", "Spock"}; // rock, paper, scissors, lizard and Spock
+        string word1, word2;
         cin >> word1 >> word2;
-        for (int i = 0; i < n; i++)
-        {
-            if (word1 == array1[i])
-            {
-                Sheldon = i;
-            }
 
-            if (word2 == array1[i])
-            {
-                raj = i;
-            }
-        }
+        cout << "Caso #" << tc << ": ";
 
-        if (Sheldon == raj)
+        if (word1 == word2)
         {
-            cout << "Caso #" << n << "De novo!" << endl;
+            cout << "De novo!" << endl;
         }
-        else if (Sheldon > raj)
+        else if ((word1 == "tesoura" && (word2 == "papel" || word2 == "lagarto")) ||
+                 (word1 == "papel" && (word2 == "pedra" || word2 == "Spock")) ||
+                 (word1 == "pedra" && (word2 == "lagarto" || word2 == "tesoura")) ||
+                 (word1 == "lagarto" && (word2 == "Spock" || word2 == "papel")) ||
+                 (word1 == "Spock" && (word2 == "tesoura" || word2 == "pedra")))
         {
-            cout << "Caso #" << n << "Bazinga!" << endl;
+            cout << "Bazinga!" << endl;
         }
         else
         {
-            cout << "Caso #" << n << "Raj trapaceou!" << endl;
+            cout << "Raj trapaceou!" << endl;
         }
     }
 
