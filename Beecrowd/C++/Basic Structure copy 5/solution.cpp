0e919edd04db59bb5026485a8e81c1e4a60c1bb3
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-07-25 13:17:17 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 5/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 5/solution.cpp
@@ -1,24 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for (n--)
     {
         int Sheldon, raj;
         string word1, word2, array1[5] = {"pedra", "papel", "tesoura", "lagarto", "Spock"};
         cin >> word1 >> word2;
         for (int i = 0; i < n; i++)
         {
-            /* code */
             if (word1 == array1[i])
             {
+                Sheldon = i;
+            }
+
+            if (word2 == array1[i])
+            {
+                raj = i;
             }
         }
+
+        if (Sheldon == raj){
+            cout << "Caso #" << n << "De novo!" << endl ;
+        }
+        else if (Sheldon>raj)
+        {
+            
+        }
+
+
+
+
     }
 
     return 0;
