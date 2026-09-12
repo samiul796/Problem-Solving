ca18080dcc21890c1f8efa220de3d5a40761b284
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-13 01:58:34 +0600

    fixed

--- Beecrowd/C++/Basic Structure copy 6/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 6/solution.cpp
@@ -5,7 +5,7 @@ int main()
 {
 
     int number;
-    while ((cin >> number) && number >= 3 && number <= 70)
+    while ((cin >> number) && number >= 3 && number < 70)
     {
 
         int arr[number][number];
@@ -16,6 +16,7 @@ int main()
 
                 if (i == ii)
                 {
+
                     arr[i][ii] = 1;
                 }
                 else if ((ii + i) == (number - 1))
@@ -27,6 +28,11 @@ int main()
                     arr[i][ii] = 3;
                 }
             }
+        }
+        if (number % 2 != 0)
+        {
+
+            arr[number / 2][number / 2] = 2;
         }
         for (int i = 0; i < number; i++)
         {
