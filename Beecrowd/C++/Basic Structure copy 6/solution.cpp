ff89e0749c17de97ba4604fdfe194ef21a54901e
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-13 01:51:30 +0600

    fixed

--- Beecrowd/C++/Basic Structure copy 6/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 6/solution.cpp
@@ -5,36 +5,39 @@ int main()
 {
 
     int number;
-
-    cin >> number;
-    int arr[number][number];
-    for (int i = 0; i < number; i++)
+    while ((cin >> number) && number >= 3 && number <= 70)
     {
-        for (int ii = 0; ii < number; ii++)
-        {
 
-            if (i == ii)
+        int arr[number][number];
+        for (int i = 0; i < number; i++)
+        {
+            for (int ii = 0; ii < number; ii++)
             {
-                arr[i][ii] = 1;
-            }
-            else if ((ii + i) == (number - 1))
-            {
-                arr[i][ii] = 2;
-            }
-            else
-            {
-                arr[i][ii] = 3;
+
+                if (i == ii)
+                {
+                    arr[i][ii] = 1;
+                }
+                else if ((ii + i) == (number - 1))
+                {
+                    arr[i][ii] = 2;
+                }
+                else
+                {
+                    arr[i][ii] = 3;
+                }
             }
         }
-    }
-    for (int i = 0; i < number; i++)
-    {
-        for (int ii = 0; ii < number; ii++)
+        for (int i = 0; i < number; i++)
         {
+            for (int ii = 0; ii < number; ii++)
+            {
 
-            cout << arr[i][ii];
+                cout << arr[i][ii];
+            }
+            cout << endl;
         }
-        cout << endl;
     }
+
     return 0;
 }
