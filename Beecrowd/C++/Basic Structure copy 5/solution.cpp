1409ae6f5656b7d47c4de156e89d34b59c383bfd
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-13 21:32:16 +0600

    d

--- Beecrowd/C++/Basic Structure copy 5/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 5/solution.cpp
@@ -1,28 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-
-    int n;
-    while (cin >> n && n != 0)
-    {
-        for (int k = 0; k < n; k++)
-        {
-            for (int l = 0; l < n; l++)
-            {
-                if (k == 0 || k == (n - 1) || l == 0 || l == (n - 1))
-                {
-                    cout << "\t" << 1;
-                }
-                else
-                {
-                    cout << "\t" << 2;
-                }
-            }
-            cout << "\n";
-        }
-    }
 
     return 0;
 }
