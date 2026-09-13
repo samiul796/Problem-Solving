86701bb1500434cf8ffc69fa1a52f920eb32d53e
MD. SAMIUL ISLAM <251-15-796@diu.edu.bd>
2026-09-13 19:13:29 +0600

    Update solution.cpp

--- Beecrowd/C++/Basic Structure copy 5/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 5/solution.cpp
@@ -4,12 +4,22 @@ using namespace std;
 int main()
 {
 
+    int n;
+    while (cin >> n && n != 0)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            for (int l = 0; l < n; l++)
+            {
+               
 
 
 
-    
-//
 
+                
+            }
+        }
+    }
 
     return 0;
 }
