35f925ce08cc2f5c89089e83f88ea9fb562b03aa
𝙼𝙳. 𝚂𝙰𝙼𝙸𝚄𝙻 𝙸𝚂𝙻𝙰𝙼 <samiulcse1@gmail.com>
2026-09-15 21:42:25 +0600

    ok

--- Beecrowd/C++/Basic Structure copy 4/solution.cpp
+++ Beecrowd/C++/Basic Structure copy 4/solution.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int main()
 {
 
+    int size ;
+    cin >> size ;
+    vector <pair<int , int >> vec(size); 
+
+    for (int  i = 0; i < vec.length; i++)
+    {
+
+vec[i].first = i;
+cin >>  vec[i].second ; 
+
+  }
+    
 
 
 
